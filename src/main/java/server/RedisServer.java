a59bfa1801eb0dec3e1edb8f4436032cfcca3afb
Dokrzz <dokpewokoma@gmail.com>
2026-09-20 08:25:37 +0200

    codecrafters submit [skip ci]

--- src/main/java/server/RedisServer.java
+++ src/main/java/server/RedisServer.java
@@ -89,6 +89,8 @@ public class RedisServer {
 
             String responseBytes = msgPrefix + responseByteCount + msgPostfix;
             responseMessage = "+PONG" + msgPostfix;
+
+            System.out.println("here at least");
         }
 
         else if(commandToArgs.containsKey(Command.ECHO)){
