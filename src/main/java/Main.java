4bcf3e7aad9edef250f156115f1e860802861a2b
Dokrzz <dokpewokoma@gmail.com>
2026-09-20 06:21:37 +0200

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,9 +4,6 @@ import java.io.IOException;
 
 class Main {
 
-
-
-
     static void main(String[] args) throws IOException {
         RedisServer redisServer = new RedisServer();
         redisServer.serve();
