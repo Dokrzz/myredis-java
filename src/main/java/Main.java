d172e723bbd7df6fc01faae265bf2897e1209ef1
Dokrzz <dokpewokoma@gmail.com>
2026-09-20 04:43:19 +0200

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -84,7 +84,7 @@ class Main {
         int responseByteCount = responseMsg.length;
 
         String responseBytes = msgPrefix + responseByteCount + msgPostfix;
-        String responseMessage = Arrays.toString(responseMsg) + msgPostfix;
+        String responseMessage = "PONG" + msgPostfix;
 
         ByteBuffer writeBuffer = ByteBuffer.wrap(responseBytes.getBytes());
         sc.write(writeBuffer);
